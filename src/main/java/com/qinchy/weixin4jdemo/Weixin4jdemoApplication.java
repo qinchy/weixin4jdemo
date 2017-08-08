312121fd0afbbf8b4dc1e7238944c2a87a6f7503
qinchy <qinchy@msn.com>
2017-08-08 15:53:39 +0800

    添加代码

--- src/main/java/com/qinchy/weixin4jdemo/Weixin4jdemoApplication.java
+++ src/main/java/com/qinchy/weixin4jdemo/Weixin4jdemoApplication.java
@@ -3,12 +3,19 @@ package com.qinchy.weixin4jdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan(value = "com.qinchy.weixin4jdemo.dao")
-public class Weixin4jdemoApplication {
+public class Weixin4jdemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Weixin4jdemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Weixin4jdemoApplication.class, args);
